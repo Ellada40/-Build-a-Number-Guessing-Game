f356dbb856f95cef58754f3e80bee5fde4a8a6e2
Ellada40 <159467855+Ellada40@users.noreply.github.com>
2025-11-24 15:29:45 +0600

    Add PostgreSQL dump for number_guess database
    This SQL file contains the dump of the 'number_guess' database, including the creation of the database, the 'users' table, and sample data inserts.

---
+++ number_guess.sql
@@ -0,0 +1,77 @@
+--
+-- PostgreSQL database dump
+--
+
+-- Dumped from database version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+-- Dumped by pg_dump version 12.22 (Ubuntu 12.22-0ubuntu0.20.04.4)
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+DROP DATABASE number_guess;
+--
+-- Name: number_guess; Type: DATABASE; Schema: -; Owner: freecodecamp
+--
+
+CREATE DATABASE number_guess WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'C.UTF-8' LC_CTYPE = 'C.UTF-8';
+
+
+ALTER DATABASE number_guess OWNER TO freecodecamp;
+
+\connect number_guess
+
+SET statement_timeout = 0;
+SET lock_timeout = 0;
+SET idle_in_transaction_session_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = on;
+SELECT pg_catalog.set_config('search_path', '', false);
+SET check_function_bodies = false;
+SET xmloption = content;
+SET client_min_messages = warning;
+SET row_security = off;
+
+SET default_tablespace = '';
+
+SET default_table_access_method = heap;
+
+--
+-- Name: users; Type: TABLE; Schema: public; Owner: freecodecamp
+--
+
+CREATE TABLE public.users (
+    username character varying(22),
+    games_played integer DEFAULT 0,
+    best_game integer
+);
+
+
+ALTER TABLE public.users OWNER TO freecodecamp;
+
+--
+-- Data for Name: users; Type: TABLE DATA; Schema: public; Owner: freecodecamp
+--
+
+INSERT INTO public.users VALUES ('user_1763975837905', 2, 443);
+INSERT INTO public.users VALUES ('user_1763975837906', 5, 288);
+INSERT INTO public.users VALUES ('Ellada', 3, 9);
+INSERT INTO public.users VALUES ('user_1763976259215', 2, 195);
+INSERT INTO public.users VALUES ('user_1763976259216', 5, 382);
+INSERT INTO public.users VALUES ('user_1763976311123', 2, 340);
+INSERT INTO public.users VALUES ('user_1763976311124', 5, 39);
+INSERT INTO public.users VALUES ('user_1763976469411', 2, 228);
+INSERT INTO public.users VALUES ('user_1763976469412', 5, 11);
+
+
+--
+-- PostgreSQL database dump complete
+--
+
